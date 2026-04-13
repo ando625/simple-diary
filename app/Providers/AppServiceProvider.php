1775b73864a6590f50d34564d1d80180dca5843f
ando.yoshie <ando.yoshie1111@gmail.com>
2026-04-13 16:41:48 +0900

    セキュリティ設定変更

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //本番環境（Renderなど）なら、URLを強制的に「HTTPS（安全な封筒）」にする
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
